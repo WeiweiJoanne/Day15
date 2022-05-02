d5ba69171d6c2635a17ae8006fcaeddeb37a2ba7
joannewei <joannewei0921@gmail.com>
2022-05-03 00:29:11 +0800

    add: post controller/判斷content是否有值

--- routes/posts.js
+++ routes/posts.js
@@ -2,18 +2,32 @@ const express = require('express');
 const router = express.Router();
 const Post = require('../models/posts');
 
-router.post('/', async(req, res, next) =>  {
+router.post('/', async (req, res, next) => {
   try {
-      /* 請在此填寫答案 */
-      // 取得來自 request body 的資料
+
+    const { content, name,image } = req.body
+
+    if (content) {
+      const addPost = await Post.create({
+        content, name, image
+      })
+      res.status(200).send({
+        status: true,
+        addPost
+      })
       
-      // 驗證是否有 content 欄位 -> 若有則使用 mongoose 語法新增資料 -> 回傳成功回應
-      //                       -> 未填寫 content 欄位 -> 回傳失敗回應
-  
-  } catch (error) {
-    res.status(400).json({
+    }else{
+      res.status(400).json({
         status: 'false',
         "message": "欄位未填寫正確，或無此 todo ID"
+      });
+    }
+
+
+  } catch (error) {
+    res.status(400).json({
+      status: 'false',
+      "message": "欄位未填寫正確，或無此 todo ID"
     });
   }
 })
