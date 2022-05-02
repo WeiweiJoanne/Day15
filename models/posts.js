cbc157a3350cc4b12f5093162495ce1e8532b93d
joannewei <joannewei0921@gmail.com>
2022-05-03 00:05:34 +0800

    add: Post Schema

--- models/posts.js
+++ models/posts.js
@@ -1,7 +1,21 @@
 const mongoose = require('mongoose');
-const postSchema = new mongoose.Schema(
+const postSchema = new mongoose.Schema({
   /* 加入欄位驗證 */
-);
+  name: {
+    type: String,
+    require: [true, "姓名需填寫"]
+  },
+  content: {
+    type: String,
+    require:[true,"貼文內容需填寫"]
+  },
+  createAt:{
+    type: Date,
+    default: Date.now
+  }
+},{
+  versionKey: false
+});
 
 const Post = mongoose.model('Post', postSchema);
 
